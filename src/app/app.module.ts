c51326d280a249af8a375bec79e6b532088f5f26
li huan <li.huan@zhtu.net>
2019-09-19 00:54:04 +0800

    modify news page  ant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { HttpClientModule } from '@angular/common/http';
 
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
@@ -22,6 +23,7 @@ import { CanDeactivateGuard } from './can-deactivate-guard.service';
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
     BrowserAnimationsModule,
     AppRoutingModule,
+    HttpClientModule
   ],
   providers: [AuthGuard, CanDeactivateGuard],
   bootstrap: [AppComponent]
