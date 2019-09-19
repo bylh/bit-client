c0060b4469d3fb3434d7270aa51cbb685dd5db39
li huan <li.huan@zhtu.net>
2019-09-20 01:13:37 +0800

    add news in discovery page

--- src/app/discovery/discovery.component.ts
+++ src/app/discovery/discovery.component.ts
@@ -2,6 +2,7 @@ import { MatSnackBar } from '@angular/material';
 import { DiscoveryService } from './discovery.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ToolsService } from '../tools/tools.service';
 
 @Component({
   selector: 'app-discovery',
@@ -15,10 +16,16 @@ export class DiscoveryComponent implements OnInit {
   public segs;
   isLoaded: boolean = false;
 
+  news: Array<Object> = [];
+  newsTypes: Array<Object> = [];
+  currentId: String  = null;
+  selectedIndex = 0;
+
   constructor(
     private snackBar: MatSnackBar,
     private route: ActivatedRoute,
     private router: Router,
+    private toolsService: ToolsService,
     private discoveryService: DiscoveryService) {
     this.route.paramMap.subscribe(params => console.log('discovery: backId', params.get('backId')))
   }
@@ -27,11 +34,29 @@ export class DiscoveryComponent implements OnInit {
     console.log('disconvery init');
     this.route.paramMap.subscribe((params) => { this.selectedId = +params.get('backId'); console.log(this.selectedId); });
 
+    this.newsTypes = await this.toolsService.getNewsTypes();
     await this.getSeg();
+  }
+
+  log(args: any[]): void {
+    console.log(args);
+  }
+
+  async getNews(id: String) {
+    try {
+      this.isLoaded = false;
+      this.currentId = id;
+      this.news = await this.toolsService.getNews(id);
+    } catch(err) {
+      console.log('getNews error', err);
+    } finally {
+      this.isLoaded = true;
+    }
   }
 
   async getSeg() {
     try {
+      this.isLoaded = false;
       this.segs = await this.discoveryService.getSeg();
       console.log(this.segs);
     } catch (err) {
